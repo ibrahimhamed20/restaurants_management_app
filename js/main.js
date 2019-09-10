8b7605fb7f58d8743d93c757534ba9d85acae351
ibrahimhamed20 <ibrahim.r.hamed@fci.helwan.edu.eg>
2019-09-10 23:09:26 +0200

    restaurant main website

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log('hello from restaurants system!');
+// toggle navbar side
+$(document).ready(function () {
+    $('.leftmenutrigger').on('click', function (e) {
+        $('.side-nav').toggleClass("open");
+        e.preventDefault();
+    });
+});
